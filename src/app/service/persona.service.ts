0d8294a268b088d76ba7fd7945942e731e840347
Jorge Ortuno <mandela_94@hotmail.com>
2022-07-08 16:16:57 -0300

    Agregado mi nombre a base de datos

--- src/app/service/persona.service.ts
+++ src/app/service/persona.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { persona } from '../model/persona.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,4 +10,8 @@ export class PersonaService {
   URL = 'http://localhost:8080/personas/';
 
   constructor(private http: HttpClient) { }
+
+  public getPersona(): Observable<persona>{
+    return this.http.get<persona>(this.URL+'traer/perfil');
+  }
 }
